83cd4ed6017a8fd7e9e9001bac6d923bcf19b476
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2023-01-22 22:14:54 +0530

    Create solution.cpp

---
+++ Max Consecutive Ones/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans =0 ;
+        
+        int count=0;
+        for(int i=0; i<nums.size();i++){
+            
+            if(nums[i]==0){
+                count=0;
+            }
+            
+            else{
+                count++;
+            }
+            
+            if(count>ans) ans=count;
+            
+        }
+        
+        return ans;
+    }
+};

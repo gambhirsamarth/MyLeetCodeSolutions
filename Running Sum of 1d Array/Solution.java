a5a5c952b251841d2590980ecaf3b48f6b8357ab
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2023-03-09 20:13:37 +0530

    Create Solution.java

---
+++ Running Sum of 1d Array/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int sum =0;
+        int n = nums.length;
+
+        for(int i=0;i<n;i++){
+            sum = sum + nums[i];
+            nums[i] = sum;
+        }
+
+        return nums;
+    }
+}

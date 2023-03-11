a316096682c8b1c9a4eecda9ff37fcd5027b80b7
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2023-03-11 15:35:08 +0530

    Create Solution.java

---
+++ Contains Duplicate/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+                if (set.contains(nums[i])) {
+                    return true;
+                }
+                set.add(nums[i]);
+        }
+        return false;
+    }
+}
+
+/*
+Time Complexity : O(N)
+Space Complexity: O(N)
+*/

166c2c06d2c36c9ef3fabab4169434f60e9fc958
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2023-06-21 11:44:04 +0530

    Create Optimal Solution.java

---
+++ Divide Array Into Equal Pairs/Optimal Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        int[] freq = new int [501];
+
+        for(int i=0; i<nums.length; i++) freq[nums[i]]++;
+        for(int i=1; i<501; i++){
+            if(freq[i]%2!=0) return false;
+        }
+        return true;
+    }
+}

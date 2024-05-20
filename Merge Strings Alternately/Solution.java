345906cd221d5e4f0508a329f264a2244dd44057
samarth <gambhirsamarth@gmail.com>
2024-05-21 00:39:40 +0530

    Optimized code

--- Merge Strings Alternately/Solution.java
+++ Merge Strings Alternately/Solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int i=0,j=0;
-        String ans = new String();
-        while(i<n || j<m){
-            if(i<n) ans+=word1.charAt(i++);
-            if(j<m) ans+=word2.charAt(j++);
+        int i = 0, j = 0;
+        StringBuilder result = new StringBuilder();
+        while (i < word1.length() || j < word2.length()) {
+            result.append(i < word1.length() ? word1.charAt(i++) : "");
+            result.append(j < word2.length() ? word2.charAt(j++) : "");
         }
-        return ans;
+        return result.toString();
     }
 }

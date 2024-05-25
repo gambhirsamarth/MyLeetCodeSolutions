18b045e9f86ce9db4199b41f485ef9a9ad18a9e4
samarth <gambhirsamarth@gmail.com>
2024-05-25 18:21:22 +0530

    better solution for "Valid Anagram"

--- src/Valid Anagram/Solution.java
+++ src/Valid Anagram/Solution.java
@@ -1,19 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int [] freq = new int[26];
+        if (s.length() != t.length()) return false;
 
-        for(int i=0; i<s.length(); i++){
-            freq[s.charAt(i)-'a']++;
-        }
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
 
-        for(int i=0; i<t.length(); i++){
-            freq[t.charAt(i)-'a']--;
-        }
-
-        for(int i=0;i<26;i++){
-            if(freq[i]!=0) return false;
-        }
-
-        return true;
+        return Arrays.equals(a, b);
     }
 }

75347bfbfc35c50c4e762107c760170edf4956a2
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2024-09-05 15:31:09 +0530

    Create Solution.java

---
+++ Convert 1D Array into 2D Array/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int[][] result = new int[m][n];
+        int p = 0;
+        if (original.length != m * n) return new int[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = original[p++];
+            }
+        }
+        return result;
+    }
+}

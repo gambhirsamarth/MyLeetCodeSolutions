259b110b9ee724ab56fabd19202d7b401133f130
Samarth Gambhir <gambhirsamarth@gmail.com>
2025-09-11 17:35:05 +0530

    optimized solution

--- src/sort-vowels-in-a-string/Solution.java
+++ src/sort-vowels-in-a-string/Solution.java
@@ -1,27 +1,33 @@
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
     public String sortVowels(String s) {
-        PriorityQueue<Character> priorityQueue = new PriorityQueue<>();
-        char[] characterArray = s.toCharArray();
-        for (char character : characterArray) {
-            if (isVolwel(character))
-                priorityQueue.add(character);
+        List<Character> vowels = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (isVolwel(s.charAt(i)))
+                vowels.add(s.charAt(i));
         }
 
-        for (int i = 0; i < characterArray.length; i++) {
-            if (!isVolwel(characterArray[i])) {
+        Collections.sort(vowels);
+
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!isVolwel(s.charAt(i))) {
+                stringBuilder.append(s.charAt(i));
                 continue;
             } else {
-                characterArray[i] = priorityQueue.poll();
+                stringBuilder.append(vowels.get(index++));
             }
         }
 
-        return new String(characterArray);
+        return stringBuilder.toString();
     }
 
     private boolean isVolwel(char character) {
-        return (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U' ||
-                character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u');
+        return (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U'
+                || character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u');
     }
 }
